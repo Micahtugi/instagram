f2cf00ff9b6e46353d750f4a31c77fae2cba7760
Micah <micahkimathi@gmail.com>
2019-06-21 16:18:47 +0300

    Created the login and the logout functionality

--- gram/models.py
+++ gram/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
-class Photo(models.Model):
-    pub_date = models.DateTimeField(auto_now_add=True)
-    photo = model.ImageField(upload_to='photos/',blank=False)
-    editor=models.ForeignKey(User,on_delete=models.CASCADE)
-    likes = models.IntegerField(default=0,null=True)
+# class Photo(models.Model):
+#     pub_date = models.DateTimeField(auto_now_add=True)
+#     photo = model.ImageField(upload_to='photos/',blank=False)
+#     editor=models.ForeignKey(User,on_delete=models.CASCADE)
+#     likes = models.IntegerField(default=0,null=True)
     
 
     
--- gram/views.py
+++ gram/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+# @login_required(login_url='/accounts/login/')
+
