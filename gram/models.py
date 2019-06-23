b694ab1f6dc7722f64e8608b746e7d2716342b96
Micah <micahkimathi@gmail.com>
2019-06-23 19:39:03 +0300

    Made migrations to the database in the manage.py file

--- gram/models.py
+++ gram/models.py
@@ -1,15 +1,33 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# class Photo(models.Model):
-#     pub_date = models.DateTimeField(auto_now_add=True)
-#     photo = model.ImageField(upload_to='photos/',blank=False)
-#     editor=models.ForeignKey(User,on_delete=models.CASCADE)
-#     likes = models.IntegerField(default=0,null=True)
-    
+class Post(models.Model):
+    location = models.CharField(max_length=100)
+    content = models.ImageField(upload_to='timeline_pics')
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    
-    
+    def __str__(self):
+        return self.location
+
+    def get_absolute_url(self):
+        return reverse('gram-home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('gram.Post', on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
 
 
 # Create your models here.
